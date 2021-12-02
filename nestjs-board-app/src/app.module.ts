3f368c69c30a2fe0b210be6f4463c8d7dc159c99
yjkim13 <sk940913@naver.com>
2021-12-03 01:08:17 +0900

    BoardsModule import

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import {BoardsModule} from './boards/boards.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    BoardsModule
+  ],
   
 })
 export class AppModule {}
