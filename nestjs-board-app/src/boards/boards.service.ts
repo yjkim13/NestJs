93a66f5d493a756a88cfc55213fb13cbdca6ccc7
yjkim13 <sk940913@naver.com>
2021-12-03 13:41:07 +0900

    모든 게시물을 가져오는 Service 만들기

--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
+
 
 @Injectable()
 export class BoardsService {
-    private boards = []; //private를 사용한 이유는 다른 컴포넌트에서 수정이 가능하기 때문이다.
+  private boards: Board[] = []; //private를 사용한 이유는 다른 컴포넌트에서 수정이 가능하기 때문이다.
 
-    getAllBoards() {
-        return this.boards;
-    
-    }
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
 }
