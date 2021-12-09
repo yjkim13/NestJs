7001b39e0eb7178c8f9db01869f7b32d0b7b36ff
yjkim13 <sk940913@naver.com>
2021-12-10 01:35:27 +0900

    typeorm 연결

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import {BoardsModule} from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule
   ],
-  
+
 })
-export class AppModule {}
+export class AppModule { }
