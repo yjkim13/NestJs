e43ce9a19b23021df9600a2a5876d928b2bbb100
yjkim13 <sk940913@naver.com>
2021-12-20 23:34:26 +0900

    Board Entity 생성

--- nestjs-board-app/src/boards/board.entity.ts
+++ nestjs-board-app/src/boards/board.entity.ts
@@ -1,4 +1,17 @@
-import { BaseEntity } from "typeorm"
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { BoardStatus } from "./board.model";
 
+@Entity()
 export class Board extends BaseEntity {
+    @PrimaryGeneratedColumn() //id 열이 Board 엔티티의 기본 키 열임을 뜻함
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
 }
