d8edf8a5698c8416239aa07b6df5ce116da4128c
yjkim13 <sk940913@naver.com>
2021-12-25 23:18:08 +0900

    회원가입 기능 구현

--- nestjs-board-app/src/auth/auth.service.ts
+++ nestjs-board-app/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRspository } from './user.repository';
 
 @Injectable()
@@ -8,4 +9,8 @@ export class AuthService {
         @InjectRepository(UserRspository)
         private userRepository: UserRspository
     ) { }
+
+    async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialDto);
+    }
 }
--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,8 +1,14 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRspository extends Repository<User>{
+    async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialDto;
+        const user = this.create({ username, password });
 
+        await this.save(user);
+    }
 
 }
