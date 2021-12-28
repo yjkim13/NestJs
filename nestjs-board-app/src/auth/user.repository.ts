54fd7fbba3d6aa2335ed055939686443d5cf2bd0
yjkim13 <sk940913@naver.com>
2021-12-29 02:44:51 +0900

    비밀번호 암호화 하기

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -2,12 +2,16 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcryptjs'
 
 @EntityRepository(User)
 export class UserRspository extends Repository<User>{
     async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialDto;
-        const user = this.create({ username, password });
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword });
 
 
         try {
