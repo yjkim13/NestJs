8a3bf1feb30982eaab3a988393a5dd594ede162e
yjkim13 <sk940913@naver.com>
2021-12-30 22:43:09 +0900

    로그인시, jwt token 생성 적용

--- nestjs-board-app/src/auth/auth.module.ts
+++ nestjs-board-app/src/auth/auth.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Type } from 'class-transformer';
+import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRspository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: "Secret1234",
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
     TypeOrmModule.forFeature([UserRspository])
   ],
   controllers: [AuthController],
--- nestjs-board-app/src/auth/auth.service.ts
+++ nestjs-board-app/src/auth/auth.service.ts
@@ -3,24 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRspository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRspository)
-        private userRepository: UserRspository
+        private userRepository: UserRspository,
+        private jwtService: JwtService
     ) { }
 
     async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialDto);
     }
 
-    async signIn(authCredentialDto: AuthCredentialsDto): Promise<string> { //type으로  string을 주기 때문에
+    async signIn(authCredentialDto: AuthCredentialsDto): Promise<{ accessToken: string }> { //type으로  string을 주기 때문에
         const { username, password } = authCredentialDto;
         const user = await this.userRepository.findOne({ username });
 
         if (user && (await bcrypt.compare(password, user.password))) {
-            return 'login success'
+            //유저 토큰을 생성 ( Secret + Payload)
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return { accessToken };
         } else {
             throw new UnauthorizedException("login failed");
         }
