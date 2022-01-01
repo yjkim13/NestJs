17d78b9a090b8f0a5c40a5930feec66c6cd0e9fd
yjkim13 <sk940913@naver.com>
2022-01-01 20:45:17 +0900

    User와 Boards 관계 형성

--- nestjs-board-app/src/auth/user.entity.ts
+++ nestjs-board-app/src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
 @Unique(['username']) //같은 이름을 가진 유저를 찾는 방법
@@ -11,4 +12,7 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    @OneToMany(type => Board, board => board.user, { eager: true }) //eager가 true면 user를 불러올떄, board 정보도 같이 불러온다. 
+    boards: Board[]
 }
